0de6b2163242d67ad95689611e2a506bb8e3d742
wzqpi <wzqpi>
2019-12-22 08:25:26 +0000

    New

--- Homework/boilerplate/main.cpp
+++ Homework/boilerplate/main.cpp
@@ -28,7 +28,8 @@ uint8_t output[2048];
 // 2: 10.0.2.1
 // 3: 10.0.3.1
 // 你可以按需进行修改，注意端序
-in_addr_t addrs[N_IFACE_ON_BOARD] = {0x0204a8c0, 0x0205a8c0, 0x0102000a,
+// new!!!!!!!!
+in_addr_t addrs[N_IFACE_ON_BOARD] = {0x0203a8c0, 0x0104a8c0, 0x0102000a,
                                      0x0103000a};
 
 int main(int argc, char *argv[]) {
